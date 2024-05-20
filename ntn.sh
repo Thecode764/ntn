6c736f8aa2f6036bbc562e7842823bea9929796f
Artin karimi <artin962354@proton.me>
2024-05-20 11:46:47 +0330

    🐛 ✨: Fix bugs and new feature

--- ntn.sh
+++ ntn.sh
@@ -1,26 +1,30 @@
 if [[ "$1" == "--help" ]]; then
     echo -e "\x1b[33mWelcome to ntn"
-    echo -e "\x1b[39m--help: Show this text\n--i: Install a package"
-    
+    echo -e "\x1b[39m--help: Show this text\n--i: Install a package\n--ri [filename]: Install a package with nr file"
+
 elif [[ "$1" == "--i" ]]; then
     npm install $2
 
 elif [[ "$1" == "--in" ]]; then
     npm install $2
 
-elif [[ "$1" == "--ins"]]; then
+elif [[ "$1" == "--ins" ]]; then
     npm install $2
 
-elif [[ "$1" == "--inst"]]; then
+elif [[ "$1" == "--inst" ]]; then
     npm install $2
 
-elif [[ "$1" == "--insta"]]; then
+elif [[ "$1" == "--insta" ]]; then
+    npm install $2
 
+elif [[ "$1" == "--instal" ]]; then
     npm install $2
-elif [[ "$1" == "--instal"]]; then
 
+elif [[ "$1" == "--install" ]]; then
     npm install $2
-elif [[ "$1" == "--install"]]; then
-    npm install $2
-    
+
+elif [[ "$1" == "--ri" ]]; then
+    code=$(cat $2)
+    npm install $code
+
 fi
