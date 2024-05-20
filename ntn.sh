984897fa6d8fb262562fda482ad8c7fcc0570e09
Artin karimi <artin962354@proton.me>
2024-05-20 11:34:08 +0330

    🧵 Add shortcuts

--- ntn.sh
+++ ntn.sh
@@ -3,4 +3,16 @@ if [[ "$1" == "--help" ]]; then
     echo -e "\x1b[39m--help: Show this text\n--i: Install a package"
 elif [[ "$1" == "--i" ]]; then
     npm install $2
+elif [[ "$1" == "--in" ]]; then
+    npm install $2
+elif [[ "$1" == "--ins"]]; then
+    npm install $2
+elif [[ "$1" == "--inst"]]; then
+    npm install $2
+elif [[ "$1" == "--insta"]]; then
+    npm install $2
+elif [[ "$1" == "--instal"]]; then
+    npm install $2
+elif [[ "$1" == "--install"]]; then
+    npm install $2
 fi
