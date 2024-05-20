a836c67fa5b26ddc18b1e41a5a21ed3946419ad3
Artin karimi <artin962354@proton.me>
2024-05-20 17:45:04 +0330

    ✨: Add new feature

--- ntn.sh
+++ ntn.sh
@@ -4,27 +4,34 @@ if [[ "$1" == "--help" ]]; then
 
 elif [[ "$1" == "--i" ]]; then
     npm install $2
+    echo -e "ntn --install $2\nNo error now\n" >> logs/log.txt
 
 elif [[ "$1" == "--in" ]]; then
     npm install $2
+    echo -e "ntn --install $2\nNo error now\n" >> logs/log.txt
 
 elif [[ "$1" == "--ins" ]]; then
     npm install $2
+    echo -e "ntn --install $2\nNo error now\n" >> logs/log.txt
 
 elif [[ "$1" == "--inst" ]]; then
     npm install $2
+    echo -e "ntn --install $2\nNo error now\n" >> logs/log.txt
 
 elif [[ "$1" == "--insta" ]]; then
     npm install $2
+    echo -e "ntn --install $2\nNo error now\n" >> logs/log.txt
 
 elif [[ "$1" == "--instal" ]]; then
     npm install $2
+    echo -e "ntn --install $2\nNo error now\n" >> logs/log.txt
 
 elif [[ "$1" == "--install" ]]; then
     npm install $2
+    echo -e "ntn --install $2\nNo error now\n" >> logs/log.txt
 
 elif [[ "$1" == "--ri" ]]; then
     code=$(cat $2)
     npm install $code
-
+    echo -e "ntn --install $code\nNo error now\n" >> logs/log.txt
 fi
