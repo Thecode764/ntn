e342f412688d435baad590d15c920c45ec38af40
Artin karimi <artin962354@proton.me>
2024-05-22 09:57:15 +0330

    🐛 Fix readthedocs bug

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,8 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes", ]
+
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
