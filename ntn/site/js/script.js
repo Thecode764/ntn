8ac0fc9c5b91cdc56de4ca31c4ff96dd9d149e42
Artin karimi <artin962354@proton.me>
2024-05-23 17:27:29 +0330

    💩 Add fun code for android and ios

---
+++ ntn/site/js/script.js
@@ -0,0 +1,23 @@
+function getOperatingSystem() {
+    var userAgent = window.navigator.userAgent,
+        platform = window.navigator.platform,
+        os = null;
+
+    if (/Mac/.test(platform)) {
+        os = 'macOS';
+    } else if (/Win/.test(platform)) {
+        os = 'Windows';
+    } else if (/Linux/.test(platform)) {
+        os = 'Linux';
+    }
+
+    if (/iPad|iPhone|iPod/.test(userAgent) &&!window.MSStream) {
+        window.location.href = 'notavailable.html'
+    } else if (/android/i.test(userAgent)) {
+        window.location.href = 'notavailable.html'
+    }
+
+    return os;
+}
+
+console.log(getOperatingSystem());
